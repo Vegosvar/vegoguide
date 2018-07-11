7f693bb46b4ad85424ca9252805b767f8287dd23
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2018-07-11 21:31:52 +0200

    Update import paths

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './components/App';
+import * as serviceWorker from './scripts/serviceWorker';
 import { Provider } from 'react-redux';
 import configureStore from './store';
+import './styles/index.scss';
 
 const store = configureStore();
 
