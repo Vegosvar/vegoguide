9d18f02c4fdff6e92a95523f55b8d95fa19f699c
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2018-07-16 16:53:14 +0200

    Export components as named exports

---
+++ src/components/index.js
@@ -0,0 +1,8 @@
+export { default as App } from './App';
+export { default as Card } from './Card';
+export { default as Col } from './Col';
+export { default as Container } from './Container';
+export { default as Page } from './Page';
+export { default as PageCard } from './PageCard';
+export { default as PageCardGrid } from './PageCardGrid';
+export { default as Row } from './Row';
