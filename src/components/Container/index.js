766ce4d5ad2a943efbefe6e17f03f19f4cd23ea9
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2018-07-17 00:31:24 +0200

    Rename default export

--- src/components/Container/index.js
+++ src/components/Container/index.js
@@ -2,10 +2,10 @@ import React from 'react';
 import classNames from 'classnames';
 import style from './style.module.scss';
 
-const Grid = ({ children }) => {
+const Container = ({ children }) => {
   const classes = classNames([style.container, style.lg]);
 
   return <div className={classes}>{children}</div>;
 };
 
-export default Grid;
+export default Container;
