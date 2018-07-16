47780f02b2ea59c0e93b4b43cabda1025f5add09
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2018-07-17 00:23:55 +0200

    Fix invalid syntax

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import style from './style.module.scss';
 
 const Loading = () => (
-  <div class={style.wrapper}>
+  <div className={style.wrapper}>
     <div className={style.loader}></div>
   </div>
 );
