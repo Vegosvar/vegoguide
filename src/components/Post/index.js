d7a3bf640b2388804488d543bb7991a3d9b935cf
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2018-07-17 01:23:50 +0200

    Update to show more post details

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
+import { Image } from 'components';
 import style from './style.module.scss';
 
-const Post = ({ label }) => (
+const Post = ({ address, cover, images, label }) => (
   <div className={style.post}>
-    {label}
+    <div className={style.cover}>
+      <Image src={cover.original} alt="" height="400px" width="100%" />
+    </div>
+    <div className={style.details}>
+      <h1 className={style.title}>{label}</h1>
+      <address className={style.address}>{address}</address>
+    </div>
   </div>
 );
 
