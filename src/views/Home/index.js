83927f06b92c33f5ed00b9008b451bfbc07cff4e
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2018-07-16 16:54:10 +0200

    Show PageCardGrid in Home view

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import PageCardGrid from 'containers/PageCardGrid';
+import { Container, Page } from 'components';
 
-const Home = () => <div>Home</div>;
+const Home = () => (
+  <Container>
+    <Page>
+      <PageCardGrid />
+    </Page>
+  </Container>
+);
 
 export default Home;
