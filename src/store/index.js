0ebd9811dd76b263b4746c5fd49fdccc76b33df7
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2018-07-16 16:31:59 +0200

    Add support for redux devtools

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,14 @@
 import modules from './modules';
 import { createStore } from 'redux';
 
+const reduxDevTools =
+  process.env.NODE_ENV === 'development' &&
+  typeof window.__REDUX_DEVTOOLS_EXTENSION__ === 'function'
+    ? window.__REDUX_DEVTOOLS_EXTENSION__()
+    : undefined;
+
 const configureStore = () => {
-  const store = createStore(modules);
+  const store = createStore(modules, reduxDevTools);
 
   // HMR support
   if (process.env.NODE_ENV !== 'production') {
