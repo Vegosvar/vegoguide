37e91b548e7b76aa93771d50c30e99a883f1217d
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2018-07-17 02:28:00 +0200

    Use the new Carousel component

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
-import { Image } from 'components';
+import { Carousel, Image } from 'components';
 import style from './style.module.scss';
 
 const Post = ({ address, cover, images, label }) => (
   <div className={style.post}>
     <div className={style.cover}>
-      <Image src={cover.original} alt="" height="400px" width="100%" />
+      <Carousel
+        active={images.findIndex(({ original }) => original === cover.original)}
+        height="350px"
+        images={images.map(({ title, original }) => ({
+          alt: title,
+          src: original
+        }))}
+      />
     </div>
     <div className={style.details}>
       <h1 className={style.title}>{label}</h1>
