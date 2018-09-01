43175ee4bb441b554d7bf4175df358af71f9932a
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2018-09-01 15:37:01 +0200

    Filter Posts to exclude any Post which does not have the categories selected in the filter

--- src/containers/PostFilter/index.js
+++ src/containers/PostFilter/index.js
@@ -8,13 +8,18 @@ const mapDispatchToProps = dispatch => ({
   setFilter: values => dispatch(SET_FILTER_CATEGORIES(values))
 });
 
-const mapStateToProps = state => ({
-  options: state.Posts.items.map(item => ({
-    label: item.label,
-    value: item.id
-  })),
-  selected: state.Posts.filter.value
-});
+const mapStateToProps = state => {
+  return {
+    options: state.Posts.items.reduce(
+      (categories, item) => [
+        ...categories,
+        ...item.categories.filter(category => !categories.includes(category))
+      ],
+      []
+    ),
+    values: state.Posts.filter.categories
+  };
+};
 
 export default connect(
   mapStateToProps,
