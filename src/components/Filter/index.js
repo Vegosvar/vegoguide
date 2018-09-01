adbffbf53ef94f1822f56fe6167ab3cdaf0c2987
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2018-09-01 15:35:20 +0200

    Use an intermediary function to add/remove value to values before calling setFilter

--- src/components/Filter/index.js
+++ src/components/Filter/index.js
@@ -3,15 +3,23 @@ import { get, isEqual } from "lodash";
 import { FilterItem } from "components";
 import style from "./style.module.scss";
 
-const Filter = ({ options, value, setFilterValue }) => {
   const isSelected = option => isEqual(get(option, "value", option), value);
+const Filter = ({ options, values, setFilter }) => {
+  const callback = value => {
+    if (values.includes(value)) {
+      const newValue = values.filter(filtervalue => filtervalue !== value);
+      return setFilter(newValue);
+    } else {
+      return setFilter([...values, value]);
+    }
+  };
 
   const filterOptions = options.map((option, index) => (
     <FilterItem
       key={index}
       option={option}
       selected={isSelected(option)}
-      callback={setFilterValue}
+      callback={callback}
     />
   ));
 
