557004524be1a7dc3be453cff875ab8f5f2a5f72
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2018-09-01 15:42:57 +0200

    Apply check for Posts matching filter.categories

--- src/containers/PostCardGrid/index.js
+++ src/containers/PostCardGrid/index.js
@@ -1,8 +1,20 @@
-import { connect } from 'react-redux';
-import { PostCardGrid } from 'components';
+import { connect } from "react-redux";
+import { PostCardGrid } from "components";
 
-const mapStateToProps = state => ({
-  items: state.Posts.items
-});
+const mapStateToProps = state => {
+  const props = {};
+
+  if (state.Posts.filter.categories.length > 0) {
+    props.items = state.Posts.items.filter(item =>
+      item.categories.some(category =>
+        state.Posts.filter.categories.includes(category)
+      )
+    );
+  } else {
+    props.items = state.Posts.items;
+  }
+
+  return props;
+};
 
 export default connect(mapStateToProps)(PostCardGrid);
