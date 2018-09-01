d9108734be242aa0b5cb605956aa995c3f98bb70
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2018-09-01 15:42:24 +0200

    Use lodash's set to ensure that the properties gets created if they do not exsits

--- src/store/modules/Posts/reducers.js
+++ src/store/modules/Posts/reducers.js
@@ -1,13 +1,14 @@
 import { CREATE_POST, SET_FILTER_CATEGORIES } from "./constants";
+import { set } from "lodash";
 
 const reducers = {
   [CREATE_POST](state, { post }) {
-    state.items.push(post);
+    set(state, "items", [...state.items, post]);
 
     return state;
   },
-    state.filter.value = value;
   [SET_FILTER_CATEGORIES](state, { value }) {
+    set(state, "filter.categories", value);
 
     return state;
   }
