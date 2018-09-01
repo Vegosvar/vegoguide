8c4bcfb75e609ebc369f332187aa3507294c2a78
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2018-09-01 14:36:12 +0200

    image has been renamed to cover

--- src/components/PostCard/index.js
+++ src/components/PostCard/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { Card, Image } from 'components';
 
-const PostCard = ({ image, label, url }) => (
+const PostCard = ({ cover, label, url }) => (
   <Link to={url}>
     <Image src={cover.thumbnail} alt={label} height="200px" width="100%" />
     <Card>
