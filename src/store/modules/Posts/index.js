e26374142b958a4da902f3b3dd21d83a9052af95
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2018-09-01 15:38:06 +0200

    Spread state in call to reducer so the reducer does not have to return a new state object but instead can modify the incoming one directly

--- src/store/modules/Posts/index.js
+++ src/store/modules/Posts/index.js
@@ -3,7 +3,7 @@ import initialState from "./state";
 
 const Posts = (state = initialState, { type, ...action }) => {
   if (type in reducers) {
-    return reducers[type](state, { ...action });
+    return reducers[type]({ ...state }, { ...action });
   }
 
   return state;
