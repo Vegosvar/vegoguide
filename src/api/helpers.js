f697fa90e88399603e1454162696ec40408af900
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2018-09-23 00:55:17 +0200

    Update helpers

--- src/api/helpers.js
+++ src/api/helpers.js
@@ -1,7 +1,20 @@
-import config from '../../config.json';
+import config from "../config.json";
 
-const {
-  host, port, protocol, token, version,
-} = config;
+const { host, port, protocol, token, version } = config.api;
 
-export const getUrl = path => `${protocol}://${host}/${version}/${path}`;
+export const getUrl = (path = "", params) => {
+  const url = new URL(
+    `${protocol}://${host}${port ? `:${port}` : ""}/${version}/${path}`
+  );
+  if (params) {
+    Object.keys(params).forEach(key =>
+      url.searchParams.append(key, JSON.stringify(params[key]))
+    );
+  }
+  return url;
+};
+
+export const getOptions = settings => ({
+  ...settings,
+  token: `JWT ${token}`
+});
