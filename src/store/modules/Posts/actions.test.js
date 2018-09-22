104d3ad9df758256b55df48e674a740ee0b44cca
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2018-09-22 20:06:55 +0200

    Format test

--- src/store/modules/Posts/actions.test.js
+++ src/store/modules/Posts/actions.test.js
@@ -1,11 +1,11 @@
-import actions from "./actions.js";
-import { SET_FILTER_CATEGORIES } from "./constants";
+import actions from './actions';
+import { SET_FILTER_CATEGORIES } from './constants';
 
-test("SET_FILTER_CATEGORIES returns correct value", () => {
-  const value = ["Snabbmat"];
+test('SET_FILTER_CATEGORIES returns correct value', () => {
+  const value = ['Snabbmat'];
   const reference = {
     type: SET_FILTER_CATEGORIES,
-    value
+    value,
   };
 
   const result = actions[SET_FILTER_CATEGORIES](value);
