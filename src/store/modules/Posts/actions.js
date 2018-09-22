ffc1d4f7ee1fdf068e392bb46ee87088474bfbd8
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2018-09-22 20:06:48 +0200

    Add FETCH_POSTS action/reducer

--- src/store/modules/Posts/actions.js
+++ src/store/modules/Posts/actions.js
@@ -1,6 +1,12 @@
-import { SET_FILTER_CATEGORIES } from "./constants";
+import { FETCH_POSTS, SET_FILTER_CATEGORIES } from "./constants";
 
 const actions = {
+  [FETCH_POSTS](value) {
+    return {
+      type: FETCH_POSTS,
+      value
+    }
+  },
   [SET_FILTER_CATEGORIES](value) {
     return {
       type: SET_FILTER_CATEGORIES,
--- src/store/modules/Posts/reducers.js
+++ src/store/modules/Posts/reducers.js
@@ -1,17 +1,27 @@
-import { CREATE_POST, SET_FILTER_CATEGORIES } from "./constants";
-import { set } from "lodash";
+import { get, set } from 'lodash';
+import { CREATE_POST, FETCH_POSTS, SET_FILTER_CATEGORIES } from './constants';
 
 const reducers = {
   [CREATE_POST](state, { post }) {
-    set(state, "items", [...state.items, post]);
+    set(state, 'items', [...state.items, post]);
+
+    return state;
+  },
+  [FETCH_POSTS](state, { posts = [] }) {
+    const path = 'sync.fetch';
+
+    const oldValue = get(state, path, []);
+    const newValue = [...oldValue, ...posts];
+
+    set(state, path, newValue);
 
     return state;
   },
   [SET_FILTER_CATEGORIES](state, { value }) {
-    set(state, "filter.categories", value);
+    set(state, 'filter.categories', value);
 
     return state;
-  }
+  },
 };
 
 export default reducers;
