a3dbaa8889338aed1bcea2df9d2039a862989279
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2018-09-23 03:46:19 +0200

    Don't invoke redux dev tools manually

--- src/store/middleware/redux-dev-tools.js
+++ src/store/middleware/redux-dev-tools.js
@@ -1,5 +1,5 @@
 import { compose } from 'redux';
 /* eslint-disable no-underscore-dangle */
 export default (typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-  ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
+  ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
   : compose);
