ecae7655675f93931ea9bd600abc0e6a5cf04740
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2018-09-23 03:31:02 +0200

    Add CREATE_POST

--- src/store/modules/Posts/actions.js
+++ src/store/modules/Posts/actions.js
@@ -1,18 +1,24 @@
-import { FETCH_POSTS, SET_FILTER_CATEGORIES } from './constants';
+import { CREATE_POST, FETCH_POSTS, SET_FILTER_CATEGORIES } from './constants';
 
 const actions = {
+  [CREATE_POST](value) {
+    return {
+      type: CREATE_POST,
+      value
+    };
+  },
   [FETCH_POSTS](value) {
     return {
       type: FETCH_POSTS,
-      value,
+      value
     };
   },
   [SET_FILTER_CATEGORIES](value) {
     return {
       type: SET_FILTER_CATEGORIES,
-      value,
+      value
     };
-  },
+  }
 };
 
 export default actions;
