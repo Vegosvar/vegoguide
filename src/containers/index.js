0c77301c9eb64b065e97104ada528361031f998c
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2018-10-01 22:57:39 +0200

    Export CreatePost

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,3 +1,4 @@
+export { default as CreatePost } from './CreatePost';
 export { default as Post } from './Post';
 export { default as PostCardGrid } from './PostCardGrid';
 export { default as PostFilterCategories } from './PostFilterCategories';
