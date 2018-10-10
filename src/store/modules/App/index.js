a74e5df179af16a75c642dd4feef083eaa280497
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2018-10-10 17:07:30 +0200

    Fix App store module

--- src/store/modules/App/index.js
+++ src/store/modules/App/index.js
@@ -1,12 +1,2 @@
-import reducers from './reducers';
-import initialState from './state';
-
-const App = (state = initialState, { type, ...action }) => {
-  if (type in reducers) {
-    return reducers[type]({ ...state }, { ...action });
-  }
-
-  return state;
-};
-
-export default App;
+export { default as initialState } from './state';
+export { default as reducers } from './reducers';
--- src/store/modules/index.js
+++ src/store/modules/index.js
@@ -1,5 +1,7 @@
+import * as App from './App';
 import * as Posts from './Posts';
 
 export {
+  App,
   Posts
 }
