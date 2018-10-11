ae6aed7243a0e93a4b2bebca7bb4fe431a7210ac
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2018-10-11 17:36:19 +0200

    Fix prop types for images

--- src/components/Post/prop-types.js
+++ src/components/Post/prop-types.js
@@ -4,18 +4,34 @@ export default {
   address: PropTypes.shape({
     city: PropTypes.string.isRequired,
     street: PropTypes.string.isRequired,
-    postcode: PropTypes.number,
+    postcode: PropTypes.number
   }).isRequired,
   cover: PropTypes.shape({
     title: PropTypes.string.isRequired,
-    original: PropTypes.string.isRequired,
-    thumbnail: PropTypes.string.isRequired
+    original: PropTypes.shape({
+      height: PropTypes.number.isRequired,
+      width: PropTypes.number.isRequired,
+      url: PropTypes.string.isRequired
+    }).isRequired,
+    thumbnail: PropTypes.shape({
+      height: PropTypes.number.isRequired,
+      width: PropTypes.number.isRequired,
+      url: PropTypes.string.isRequired
+    }).isRequired
   }).isRequired,
   images: PropTypes.arrayOf(
     PropTypes.shape({
       title: PropTypes.string.isRequired,
-      original: PropTypes.string.isRequired,
-      thumbnail: PropTypes.string.isRequired
+      original: PropTypes.shape({
+        height: PropTypes.number.isRequired,
+        width: PropTypes.number.isRequired,
+        url: PropTypes.string.isRequired
+      }).isRequired,
+      thumbnail: PropTypes.shape({
+        height: PropTypes.number.isRequired,
+        width: PropTypes.number.isRequired,
+        url: PropTypes.string.isRequired
+      }).isRequired
     })
   ).isRequired,
   label: PropTypes.string.isRequired
