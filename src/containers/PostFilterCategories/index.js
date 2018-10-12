457284b42dbb1c058b638f81ce2e1ca8731b7d1c
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2018-10-12 13:58:00 +0200

    Provide the options' label and value property here

--- src/containers/PostFilterCategories/index.js
+++ src/containers/PostFilterCategories/index.js
@@ -3,13 +3,18 @@ import { Filter } from 'components';
 import { setFilterCategories } from 'store/modules/Posts/actions';
 
 const mapStateToProps = state => ({
-  options: state.Posts.items.reduce(
-    (categories, item) => [
-      ...categories,
-      ...item.categories.filter(category => !categories.includes(category))
-    ],
-    []
-  ),
+  options: state.Posts.items
+    .reduce(
+      (categories, item) => [
+        ...categories,
+        ...item.categories.filter(category => !categories.includes(category))
+      ],
+      []
+    )
+    .map(category => ({
+      label: category.title,
+      value: category._id
+    })),
   values: state.Posts.filter.categories
 });
 
