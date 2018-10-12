b0cbc2e0d4689f3dd6216fa5fc9318943f8cdb09
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2018-10-12 22:51:53 +0200

    Add row props

--- src/components/Row/index.js
+++ src/components/Row/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import classnames from 'classnames';
 import style from './style.module.scss';
+import defaultProps from './default-props';
+import propTypes from './prop-types';
 
-const Row = ({ children }) => (
-  <div className={style.row}>
+const Row = ({ children, gapless }) => (
+  <div
+    className={classnames([
+      style.row,
+      {
+        [style.gapless]: gapless
+      }
+    ])}
+  >
     {children}
   </div>
 );
+
+Row.defaultProps = defaultProps;
+Row.propTypes = propTypes;
 
 export default Row;
