630c01c48ba924b0c68bf603025da02ff380c765
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2018-10-12 15:32:26 +0200

    Fix proptypes

--- src/components/PostCardGrid/prop-types.js
+++ src/components/PostCardGrid/prop-types.js
@@ -2,5 +2,5 @@ import PropTypes from 'prop-types';
 import postPropTypes from 'components/Post/prop-types';
 
 export default {
-  items: PropTypes.arrayOf(postPropTypes).isRequired
+  items: PropTypes.arrayOf(PropTypes.shape(postPropTypes)).isRequired
 };
