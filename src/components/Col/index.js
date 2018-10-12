4839b61b9f484beebab781a66424d2995e6e3a2c
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2018-10-13 01:55:03 +0200

    Pass down the rest of the props down to the element

--- src/components/Col/index.js
+++ src/components/Col/index.js
@@ -3,10 +3,14 @@ import classnames from 'classnames';
 import defaultProps from './default-props';
 import propTypes from './prop-types';
 
-const Col = ({ children, size }) => {
+const Col = ({ children, size, ...props }) => {
   const classNames = classnames(['column', `col-${size}`]);
 
-  return <div className={classNames}>{children}</div>;
+  return (
+    <div className={classNames} {...props}>
+      {children}
+    </div>
+  );
 };
 
 Col.defaultProps = defaultProps;
