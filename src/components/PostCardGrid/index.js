fbd3a35d64245ef63fac44c2d02c78adb949365f
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2018-10-12 23:09:39 +0200

    Added FLIP animation for posts in grid

--- src/components/PostCardGrid/index.js
+++ src/components/PostCardGrid/index.js
@@ -1,19 +1,30 @@
 import React from 'react';
 import { Col, PostCard, Row } from 'components';
-import defaultProps from './default-props'
-import propTypes from './prop-types'
+import { Flipper, Flipped } from 'react-flip-toolkit';
+import defaultProps from './default-props';
+import propTypes from './prop-types';
 
-const PostCardGrid = ({ items }) => (
-  <Row>
-    {items.map((item, index) => (
-      <Col key={index}>
-        <PostCard {...item} />
-      </Col>
-    ))}
-  </Row>
+const FlipWrapper = ({ children, ...props }) => (
+  <div {...props}>{children}</div>
 );
 
-PostCard.defaultProps = defaultProps
+const PostCardGrid = ({ items }) => (
+  <Flipper flipKey={items.length}>
+    <Row>
+      {items.map(item => (
+        <Col size={6} key={item._id}>
+          <Flipped flipId={item._id} {...item}>
+            <FlipWrapper>
+              <PostCard {...item} />
+            </FlipWrapper>
+          </Flipped>
+        </Col>
+      ))}
+    </Row>
+  </Flipper>
+);
+
+PostCard.defaultProps = defaultProps;
 PostCardGrid.propTypes = propTypes;
 
 export default PostCardGrid;
