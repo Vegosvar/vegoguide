8ca5b1dcda4e1bde515d06838c827c11d39c3794
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2018-10-13 01:56:34 +0200

    Use animejs with react-flip-toolkit to have items fade and FLIP

--- src/components/PostCardGrid/index.js
+++ src/components/PostCardGrid/index.js
@@ -1,24 +1,47 @@
 import React from 'react';
 import { Col, PostCard, Row } from 'components';
 import { Flipper, Flipped } from 'react-flip-toolkit';
+import anime from 'animejs';
 import defaultProps from './default-props';
 import propTypes from './prop-types';
 
-const FlipWrapper = ({ children, ...props }) => (
-  <div {...props}>{children}</div>
-);
+/* eslint-disable */
+
+const onElementAppear = (el, index) => {
+  anime({
+    targets: el,
+    opacity: [0, 1],
+    delay: index * 20,
+    duration: 150,
+    easing: 'easeOutSine'
+  });
+};
+
+const onElementExit = (el, index, removeElement) => {
+  anime({
+    targets: el,
+    opacity: 0,
+    duration: 150,
+    complete: removeElement,
+    delay: index * 20,
+    easing: 'easeOutSine'
+  });
+};
 
 const PostCardGrid = ({ items }) => (
   <Flipper flipKey={items.length}>
     <Row>
-      {items.map(item => (
-        <Col size={6} key={item._id}>
-          <Flipped flipId={item._id} {...item}>
-            <FlipWrapper>
-              <PostCard {...item} />
-            </FlipWrapper>
-          </Flipped>
-        </Col>
+      {items.map((item, i) => (
+        <Flipped
+          flipId={item._id}
+          onDelayedAppear={onElementAppear}
+          onExit={onElementExit}
+          key={item._id}
+        >
+          <Col size={6}>
+            <PostCard {...item} />
+          </Col>
+        </Flipped>
       ))}
     </Row>
   </Flipper>
