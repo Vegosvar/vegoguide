1a60383e9b29bf3499c2aa6f313a3c2d697d14c5
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2018-10-17 02:57:16 +0200

    use the reactI18nextModule

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,26 +1,30 @@
 import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
+import { reactI18nextModule } from 'react-i18next';
 import * as sv from './sv';
 
 // TODO: Yay, first TODO! Move the locales to the backend and fetch and cache them
 
-i18n.use(LanguageDetector).init({
-  resources: {
-    sv
-  },
-  debug: false,
-  fallbackLng: 'en',
-  preload: ['sv', 'en'],
-  interpolation: {
-    escapeValue: false
-  },
-  defaultNS: 'common',
-  react: {
-    wait: false,
-    bindI18n: 'languageChanged loaded',
-    bindStore: 'added removed',
-    nsMode: 'default'
-  }
-});
+i18n
+  .use(reactI18nextModule)
+  .use(LanguageDetector)
+  .init({
+    resources: {
+      sv
+    },
+    debug: false,
+    fallbackLng: 'en',
+    preload: ['sv', 'en'],
+    interpolation: {
+      escapeValue: false
+    },
+    defaultNS: 'common',
+    react: {
+      wait: false,
+      bindI18n: 'languageChanged loaded',
+      bindStore: 'added removed',
+      nsMode: 'default'
+    }
+  });
 
 export default i18n;
