e39a35b4f3171e27de0010cc497d2c9e734b9bce
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2018-10-17 02:49:39 +0200

    Use Image component to show image

--- src/components/ImagePreview/index.js
+++ src/components/ImagePreview/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Image } from 'components';
 import propTypes from './prop-types';
 import style from './style.module.scss';
 
 const ImagePreview = ({ file, index }) => (
     <img alt={file.name} src={file.preview} />
   <div className={style.preview} key={`${file.name}-${index}`} {...props}>
+    <Image alt={file.name} src={file.preview} />
   </div>
 );
 
