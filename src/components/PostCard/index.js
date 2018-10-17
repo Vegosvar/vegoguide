d7486af4847c580de6539ef82dcd99fe588d807f
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2018-10-17 14:36:59 +0200

    Wrap Image in CardImage component

--- src/components/PostCard/index.js
+++ src/components/PostCard/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Card, CardBody, Image } from 'components';
+import { Card, CardBody, CardImage, Image } from 'components';
 import propTypes from './prop-types';
 
 const PostCard = ({ cover, title, url }) => (
   <Link to={url}>
-    <Image src={cover.thumbnail.url} alt={title} height="200px" width="100%" />
     <Card>
+      <CardImage>
+        <Image
+          src={cover.thumbnail.url}
+          alt={title}
+          height="200px"
+          width="100%"
+        />
+      </CardImage>
       <CardBody>{title}</CardBody>
     </Card>
   </Link>
