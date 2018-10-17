d6ba70d78623d157d6bb3453b420955807ef21c4
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2018-10-17 03:01:37 +0200

    Add Navbar

--- src/views/Home/index.js
+++ src/views/Home/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { PostCardGrid } from 'containers';
-import { Container, Hero, Page } from 'components';
+import { Container, Hero, Navbar, Page } from 'components';
 
 const Home = () => [
+  <Navbar key="navbar" />,
   <Hero key="hero" />,
   <Container key="container">
     <Page>
