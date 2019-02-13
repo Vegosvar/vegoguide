40b021f5c9201b4c0fedad1f47b1bd34ac1508c1
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2019-02-13 19:13:07 +0100

    Update fetching status when a new fetch is initialized / completed and handle errors

--- src/store/modules/Posts/sagas/fetch-posts.js
+++ src/store/modules/Posts/sagas/fetch-posts.js
@@ -1,17 +1,30 @@
 import Api from 'api';
 import { all, call, put, takeLatest } from 'redux-saga/effects';
 import { FETCH_POSTS } from '../constants';
-import { createPost } from '../actions';
+import { createPost, setError, setFetching } from '../actions';
 
 function* worker({ params, settings } = {}) {
-  // Call the API and fetch the posts
-  const response = yield call(Api.Posts.fetch, params, settings);
+  // Clear last error
+  yield put(setError(null));
 
-  // Parse the response as JSON
-  const { data = [] } = yield response.json();
+  // Set as fetching
+  yield put(setFetching(true));
 
-  // Loop over posts and put them in the store
-  yield all(data.map(post => put(createPost(post))));
+  try {
+    // Call the API and fetch the posts
+    const response = yield call(Api.Posts.fetch, params, settings);
+
+    // Parse the response as JSON
+    const { data = [] } = yield response.json();
+
+    // Loop over posts and put them in the store
+    yield all(data.map(post => put(createPost(post))));
+  } catch (error) {
+    yield put(setError(error.message));
+  } finally {
+    // Set as done fetching
+    yield put(setFetching(false));
+  }
 }
 
 function* watcher() {
