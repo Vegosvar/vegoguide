2a3a5a0b390a3998aeb70c8f9154e2fb41dce899
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2019-02-13 12:29:09 +0100

    Update URL search query param when query changes

--- src/containers/PostFilterSearch/index.js
+++ src/containers/PostFilterSearch/index.js
@@ -1,15 +1,37 @@
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router';
+import { push } from 'connected-react-router';
 import { Input } from 'components';
 import { setFilterSearch } from 'store/modules/Posts/actions';
+import queryString from 'query-string';
 import PropTypes from './prop-types';
 
+const parseQuery = (location, property, key) => {
+  const parsed = queryString.parse(location[property]);
+  return parsed[key];
+};
+
 const mapStateToProps = state => ({
-  value: state.Posts.filter.search
+  value:
+    state.Posts.filter.search ||
+    parseQuery(state.router.location, 'search', 'search')
 });
 
 const mapDispatchToProps = dispatch => ({
-  onChange: e => dispatch(setFilterSearch(e.target.value))
+  onChange: e => {
+    const { value = '' } = e.target;
+
+    dispatch(
+      push({
+        search: `?search=${value}`
+      })
+    );
+
+    dispatch(setFilterSearch(value));
+  }
 });
+
+// TODO: We need to dispatch the setFilterSearch value when the component is loaded if value is already set
 
 const PostFilterSearch = connect(
   mapStateToProps,
