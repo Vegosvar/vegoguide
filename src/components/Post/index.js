e7d54f1d15b7128c30702f1511a78548b964e66c
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2019-02-14 13:52:44 +0100

    Format

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -12,9 +12,7 @@ const Post = ({
   <div className={style.post}>
     <div className={style.cover}>
       <Carousel
-        active={images.findIndex(
-          image => image._id === cover._id
-        )}
+        active={images.findIndex(image => image._id === cover._id)}
         height="350px"
         images={images.map(image => ({
           alt: image.title,
