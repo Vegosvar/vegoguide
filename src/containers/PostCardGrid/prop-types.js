06c03a01c96986e0a8a6316c079dd23be46f0ab5
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2019-06-18 17:12:02 +0200

    Fix invalid prop types

--- src/containers/PostCardGrid/prop-types.js
+++ src/containers/PostCardGrid/prop-types.js
@@ -4,6 +4,6 @@ import { propTypes as PostPropTypes } from 'components/Post';
 export default {
   fetch: PropTypes.func.isRequired,
   isFetching: PropTypes.bool.isRequired,
-  posts: PropTypes.arrayOf(PostPropTypes).isRequired,
-  filteredPosts: PropTypes.arrayOf(PostPropTypes).isRequired
+  posts: PropTypes.arrayOf(PropTypes.shape(PostPropTypes)).isRequired,
+  filteredPosts: PropTypes.arrayOf(PropTypes.shape(PostPropTypes)).isRequired
 };
