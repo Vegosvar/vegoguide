4f5b2dd06c81e6a85410a32bb6182ea23b6cfcf7
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2019-06-25 23:08:12 +0200

    Add an array of breakpoints for window width and how many columns a view should show

--- src/store/modules/App/state.js
+++ src/store/modules/App/state.js
@@ -1,4 +1,22 @@
 const state = {
+  breakpoints: [
+    {
+      value: 540,
+      columnSize: 12
+    },
+    {
+      value: 720,
+      columnSize: 12
+    },
+    {
+      value: 960,
+      columnSize: 6
+    },
+    {
+      value: 1140,
+      columnSize: 4
+    }
+  ],
   isSyncing: false,
   isSynced: false,
   syncedDate: 0,
