35ea88021cc2697ad1c96ee84a1e9212c80b9141
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2019-06-25 21:20:22 +0200

    Add constant for setting screen height and width

--- src/store/modules/App/constants.js
+++ src/store/modules/App/constants.js
@@ -3,4 +3,6 @@ export const SET_SYNCED = 'SET_SYNCED';
 export const SET_SYNCING = 'SET_SYNCING';
 export const SET_SYNCED_DATE = 'SET_SYNCED_DATE';
 export const SET_LANGUAGE = 'SET_LANGUAGE';
+export const SET_SCREEN_HEIGHT = 'SET_SCREEN_HEIGHT';
+export const SET_SCREEN_WIDTH = 'SET_SCREEN_WIDTH';
 export const SET_URL_QUERY_PARAM = 'SET_URL_QUERY_PARAM';
