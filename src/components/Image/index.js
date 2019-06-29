5f863fd841fe0e9c31d1f02062069fe05acf59f0
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2019-06-29 22:00:11 +0200

    Add support for src-set and sizes attributes

--- src/components/Image/index.js
+++ src/components/Image/index.js
@@ -5,11 +5,23 @@ import classnames from 'classnames';
 import defaultProps from './default-props';
 import propTypes from './prop-types';
 
-const Image = ({ alt, className, height, fallback, src, width, ...props }) => (
+const Image = ({
+  alt,
+  className,
+  height,
+  fallback,
+  src,
+  srcSet,
+  sizes,
+  width,
+  ...props
+}) => (
   <Img
     alt={alt}
     className={classnames(['img-responsive', 'img-fit-cover', className])}
     src={src}
+    src-set={srcSet}
+    sizes={sizes}
     unloader={fallback}
     loader={<Loading height={height} />}
     style={{
