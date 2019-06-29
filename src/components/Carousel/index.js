5b2071abc90db5115febaafd372bffc864cc3e7f
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2019-06-29 23:44:26 +0200

    Rewrite Carousel component to use react-carousel

--- src/components/Carousel/index.js
+++ src/components/Carousel/index.js
@@ -1,80 +1,57 @@
-import React from 'react';
+import React, { useState } from 'react';
 import classnames from 'classnames';
+import ReactCarousel, { Dots } from '@brainhubeu/react-carousel';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
   faChevronLeft,
   faChevronRight
 } from '@fortawesome/free-solid-svg-icons';
 import Image from 'components/Image';
-import Label from 'components/Label';
 import propTypes from './prop-types';
 import defaultProps from './default-props';
 import style from './style.module.scss';
+import '@brainhubeu/react-carousel/lib/style.css';
 
-const Carousel = ({ active, height, images }) => {
-  const inputs = images.map((image, index) => (
-    <input
-      className="carousel-locator"
-      id={`slide-${index}`}
-      type="radio"
-      name="carousel-radio"
-      hidden="hidden"
-      defaultChecked={index === active ? 'checked' : undefined}
-      key={`${image.src}-${index}`}
-    />
-  ));
+const Carousel = ({ active, images }) => {
+  const [carouselIndex, setCarouselIndex] = useState(active);
 
-  const items = images.map((image, index) => {
-    const prevIndex = index === 0 ? images.length - 1 : index - 1;
-    const nextIndex = index !== images.length - 1 ? index + 1 : 0;
+  const arrowLeft = (
+    <div className={classnames(style.arrow, style.left)}>
+      <FontAwesomeIcon className={style.icon} fixedWidth icon={faChevronLeft} />
+    </div>
+  );
 
-    return (
-      <figure
-        className={classnames([
-          'carousel-item',
-          active === index ? style.noAnimation : undefined
-        ])}
-        key={`${image.src}-${index}`}
-      >
-        {images.length > 1 ? (
-          <React.Fragment>
-            <Label className="item-prev" htmlFor={`slide-${prevIndex}`}>
-              <FontAwesomeIcon icon={faChevronLeft} />
-            </Label>
-            <Label className="item-next" htmlFor={`slide-${nextIndex}`}>
-              <FontAwesomeIcon icon={faChevronRight} />
-            </Label>
-          </React.Fragment>
-        ) : (
-          ''
-        )}
-        <Image {...image} width="100%" height={height} />
-      </figure>
-    );
-  });
-
-  const navigation =
-    images.length > 1 ? (
-      <div className="carousel-nav">
-        {images.map((image, index) => (
-          <Label
-            className="nav-item"
-            htmlFor={`slide-${index}`}
-            key={`${image.src}-${index}`}
-          >
-            {index}
-          </Label>
-        ))}
-      </div>
-    ) : (
-      ''
-    );
+  const arrowRight = (
+    <div className={classnames(style.arrow, style.right)}>
+      <FontAwesomeIcon
+        className={style.icon}
+        fixedWidth
+        icon={faChevronRight}
+      />
+    </div>
+  );
 
   return (
-    <div className="carousel" style={{ height }}>
-      {inputs}
-      <div className="carousel-container">{items}</div>
-      {navigation}
+    <div className={style.wrapper}>
+      <ReactCarousel
+        className={style.carousel}
+        arrowLeft={arrowLeft}
+        arrowRight={arrowRight}
+        value={carouselIndex}
+        onChange={setCarouselIndex}
+        addArrowClickHandler
+        infinite
+      >
+        {images.map((image, index) => (
+          <Image key={`${image.src}-${index}`} {...image} />
+        ))}
+      </ReactCarousel>
+      <Dots
+        className={style.dots}
+        number={images.length}
+        value={carouselIndex}
+        onChange={setCarouselIndex}
+      />
     </div>
   );
 };
--- src/components/Carousel/prop-types.js
+++ src/components/Carousel/prop-types.js
@@ -2,6 +2,5 @@ import PropTypes from 'prop-types';
 
 export default {
   active: PropTypes.number,
-  height: PropTypes.string,
   images: PropTypes.array
 };
