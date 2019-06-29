af8665fce755b1c22d1e24cc1cb670a7bd4fd799
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2019-06-29 21:46:51 +0200

    Move out prop types for images sizes used in images/cover property to its own variable

--- src/components/Post/prop-types.js
+++ src/components/Post/prop-types.js
@@ -1,38 +1,26 @@
 import PropTypes from 'prop-types';
 
+const ImageSizesPropTypes = PropTypes.shape({
+  title: PropTypes.string.isRequired,
+  original: PropTypes.shape({
+    height: PropTypes.number.isRequired,
+    width: PropTypes.number.isRequired,
+    url: PropTypes.string.isRequired
+  }).isRequired,
+  thumbnail: PropTypes.shape({
+    height: PropTypes.number.isRequired,
+    width: PropTypes.number.isRequired,
+    url: PropTypes.string.isRequired
+  }).isRequired
+});
+
 export default {
   address: PropTypes.shape({
     city: PropTypes.string.isRequired,
     street: PropTypes.string.isRequired,
     postcode: PropTypes.number
   }).isRequired,
-  cover: PropTypes.shape({
-    title: PropTypes.string.isRequired,
-    original: PropTypes.shape({
-      height: PropTypes.number.isRequired,
-      width: PropTypes.number.isRequired,
-      url: PropTypes.string.isRequired
-    }).isRequired,
-    thumbnail: PropTypes.shape({
-      height: PropTypes.number.isRequired,
-      width: PropTypes.number.isRequired,
-      url: PropTypes.string.isRequired
-    }).isRequired
-  }).isRequired,
-  images: PropTypes.arrayOf(
-    PropTypes.shape({
-      title: PropTypes.string.isRequired,
-      original: PropTypes.shape({
-        height: PropTypes.number.isRequired,
-        width: PropTypes.number.isRequired,
-        url: PropTypes.string.isRequired
-      }).isRequired,
-      thumbnail: PropTypes.shape({
-        height: PropTypes.number.isRequired,
-        width: PropTypes.number.isRequired,
-        url: PropTypes.string.isRequired
-      }).isRequired
-    })
-  ).isRequired,
+  cover: ImageSizesPropTypes.isRequired,
+  images: PropTypes.arrayOf(ImageSizesPropTypes).isRequired,
   title: PropTypes.string.isRequired
 };
