ec62f66c454bcbf04bc20b9200e59180cd7f97d7
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2019-06-29 22:00:00 +0200

    Refactor

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -8,24 +8,25 @@ const Post = ({
   cover,
   images,
   title
-}) => (
-  <div className={style.post}>
-    <div className={style.cover}>
-      <Carousel
-        active={images.findIndex(image => image._id === cover._id)}
-        height="350px"
-        images={images.map(image => ({
-          alt: image.title,
-          src: image.original.url
-        }))}
-      />
+}) => {
+  const active = images.findIndex(image => image._id === cover._id);
+  const carouselImages = images.map(image => ({
+    alt: image.title,
+    src: image.large.url
+  }));
+
+  return (
+    <div className={style.post}>
+      <div className={style.cover}>
+        <Carousel active={active} images={carouselImages} height="350px" />
+      </div>
+      <div className={style.details}>
+        <h1 className={style.title}>{title}</h1>
+        <address className={style.address}>{(street, postcode, city)}</address>
+      </div>
     </div>
-    <div className={style.details}>
-      <h1 className={style.title}>{title}</h1>
-      <address className={style.address}>{(street, postcode, city)}</address>
-    </div>
-  </div>
-);
+  );
+};
 
 Post.propTypes = propTypes;
 
