87c7c7679d1168b08026b441043cf94ca85792dd
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2019-07-03 10:39:08 +0200

    Remove static translations

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,7 +1,6 @@
 import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
-import * as sv from './sv';
 
 // TODO: Yay, first TODO! Move the locales to the backend and fetch and cache them
 
@@ -9,8 +8,6 @@ i18n
   .use(initReactI18next)
   .use(LanguageDetector)
   .init({
-    resources: {
-      sv
     },
     debug: false,
     fallbackLng: 'en',
--- src/i18n/sv/index.js
+++
@@ -1,3 +0,0 @@
-export { default as common } from './common.json';
-export { default as languages } from './languages.json';
-export { default as postCategories } from './post-categories.json';
