823cd426f337e685b17f9e29b356cadc5f25bbf1
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2019-07-03 16:09:51 +0200

    use useCallback to memoize the functions and only update the height, width and breakpoint when they are changed

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,24 +1,50 @@
-import React, { Suspense } from 'react';
-import { useDispatch } from 'react-redux';
+import React, { Suspense, useCallback } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { getBreakpoint, getScreenProperties } from 'hooks';
 import {
   setBreakpoint,
   setScreenHeight,
   setScreenWidth
 } from 'store/modules/App/actions';
+import { isEqual } from 'lodash';
 import App from 'components/App';
 import Loading from 'components/Loading';
 import propTypes from './prop-types';
 
 const AppContainer = ({ children }) => {
   const dispatch = useDispatch();
+  const currentHeight = useSelector(state => state.App.screen.height);
+  const currentWidth = useSelector(state => state.App.screen.width);
 
   const { height, width } = getScreenProperties();
-  dispatch(setScreenHeight(height));
-  dispatch(setScreenWidth(width));
+
+  const updateHeight = useCallback(() => dispatch(setScreenHeight(height)), [
+    dispatch,
+    height
+  ]);
+  const updateWidth = useCallback(() => dispatch(setScreenWidth(width)), [
+    dispatch,
+    width
+  ]);
+
+  if (currentHeight !== height) {
+    updateHeight(height);
+  }
+
+  if (currentWidth !== width) {
+    updateWidth(width);
+  }
 
   const breakpoint = getBreakpoint();
-  dispatch(setBreakpoint(breakpoint));
+  const currentBreakpoint = useSelector(state => state.App.breakpoint);
+  const updateBreakPoint = useCallback(
+    () => dispatch(setBreakpoint(breakpoint)),
+    [dispatch, breakpoint]
+  );
+
+  if (!isEqual(currentBreakpoint, breakpoint)) {
+    updateBreakPoint();
+  }
 
   return (
     <Suspense fallback={<Loading height="100%" />}>
