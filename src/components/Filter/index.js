141e4d86149ac8356498446af3a67185539e0514
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2019-07-03 20:50:22 +0200

    Add animated transitions like in PostCardGrid

--- src/components/Filter/index.js
+++ src/components/Filter/index.js
@@ -2,9 +2,30 @@ import React from 'react';
 import { get } from 'lodash';
 import classnames from 'classnames';
 import FilterItem from 'components/FilterItem';
+import { Flipper, Flipped } from 'react-flip-toolkit';
+import anime from 'animejs';
 import style from './style.module.scss';
 import propTypes from './prop-types';
 import defaultProps from './default-props';
+
+const onElementAppear = (el, index) =>
+  anime({
+    targets: el,
+    opacity: [0, 1],
+    delay: index * 20,
+    duration: 150,
+    easing: 'easeOutSine'
+  });
+
+const onElementExit = (el, index, removeElement) =>
+  anime({
+    targets: el,
+    opacity: 0,
+    duration: 150,
+    complete: removeElement,
+    delay: index * 20,
+    easing: 'easeOutSine'
+  });
 
 const Filter = ({
   className,
@@ -36,19 +57,29 @@ const Filter = ({
     return multiple ? value.includes(optionValue) : value === optionValue;
   };
 
-  const filterOptions = options.map((option, index) => (
-    <FilterItem
-      key={index}
-      option={option}
-      selected={isSelected(option)}
-      onChange={onChangeItem}
-    />
+  const filterOptions = options.map(option => (
+    <Flipped
+      flipId={option.value}
+      onDelayedAppear={onElementAppear}
+      onExit={onElementExit}
+      key={option.value}
+    >
+      <FilterItem
+        option={option}
+        selected={isSelected(option)}
+        onChange={onChangeItem}
+      />
+    </Flipped>
   ));
 
   return (
-    <div className={classnames([style.filter, className])} {...props}>
+    <Flipper
+      className={classnames([style.filter, className])}
+      flipKey={options.length}
+      {...props}
+    >
       {filterOptions}
-    </div>
+    </Flipper>
   );
 };
 
