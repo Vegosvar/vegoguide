477bf93037f806e981f980133918ab66aa9ef5bd
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2019-07-03 17:31:36 +0200

    Forgot about useEffect... that is what should have been used from the start

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,50 +1,40 @@
-import React, { Suspense, useCallback } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
+import React, { Suspense, useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 import { getBreakpoint, getScreenProperties } from 'hooks';
 import {
   setBreakpoint,
   setScreenHeight,
   setScreenWidth
 } from 'store/modules/App/actions';
-import { isEqual } from 'lodash';
 import App from 'components/App';
 import Loading from 'components/Loading';
 import propTypes from './prop-types';
 
 const AppContainer = ({ children }) => {
   const dispatch = useDispatch();
-  const currentHeight = useSelector(state => state.App.screen.height);
-  const currentWidth = useSelector(state => state.App.screen.width);
 
   const { height, width } = getScreenProperties();
 
-  const updateHeight = useCallback(() => dispatch(setScreenHeight(height)), [
-    dispatch,
-    height
-  ]);
-  const updateWidth = useCallback(() => dispatch(setScreenWidth(width)), [
-    dispatch,
-    width
-  ]);
-
-  if (currentHeight !== height) {
-    updateHeight(height);
-  }
-
-  if (currentWidth !== width) {
-    updateWidth(width);
-  }
-
-  const breakpoint = getBreakpoint();
-  const currentBreakpoint = useSelector(state => state.App.breakpoint);
-  const updateBreakPoint = useCallback(
-    () => dispatch(setBreakpoint(breakpoint)),
-    [dispatch, breakpoint]
+  useEffect(
+    () => {
+      dispatch(setScreenHeight(height));
+    },
+    [dispatch, height]
+  );
+  useEffect(
+    () => {
+      dispatch(setScreenWidth(width));
+    },
+    [dispatch, width]
   );
 
-  if (!isEqual(currentBreakpoint, breakpoint)) {
-    updateBreakPoint();
-  }
+  const breakpoint = getBreakpoint();
+  useEffect(
+    () => {
+      dispatch(setBreakpoint(breakpoint));
+    },
+    [dispatch, breakpoint]
+  );
 
   return (
     <Suspense fallback={<Loading height="100%" />}>
