cda689cac70ab2ce3c30978dabce1dce358d08e1
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2019-07-03 10:44:32 +0200

    Add spaces for better legibility

--- src/api/helpers.js
+++ src/api/helpers.js
@@ -6,11 +6,13 @@ export const getUrl = (path = '', params) => {
   const url = new URL(
     `${protocol}://${host}${port ? `:${port}` : ''}${prefix ? `/${prefix}` : ''}/${version}/${path}`
   );
+
   if (params) {
     Object.keys(params).forEach(key =>
       url.searchParams.append(key, JSON.stringify(params[key]))
     );
   }
+
   return url;
 };
 
