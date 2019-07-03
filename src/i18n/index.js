b24067f88fa7847c8e080f9c173baa111235a21c
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2019-07-03 10:39:39 +0200

    Add XHR backend and the config it needs to work

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,23 +1,29 @@
 import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
+import XHR from 'i18next-xhr-backend';
 
 // TODO: Yay, first TODO! Move the locales to the backend and fetch and cache them
 
 i18n
-  .use(initReactI18next)
+  .use(XHR)
   .use(LanguageDetector)
+  .use(initReactI18next)
   .init({
+    backend: {
+      addPath: '/i18n/{{lng}}/{{ns}}',
+      loadPath: '/i18n/{{lng}}/{{ns}}'
     },
     debug: false,
     fallbackLng: 'en',
-    preload: ['sv', 'en'],
+    preload: ['sv'],
     interpolation: {
       escapeValue: false
     },
+    saveMissing: true,
     defaultNS: 'common',
     react: {
-      wait: false,
+      wait: true,
       bindI18n: 'languageChanged loaded',
       bindStore: 'added removed',
       nsMode: 'default'
