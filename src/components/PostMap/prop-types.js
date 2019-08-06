b8a5006300d07b7c2d73b8b09ec9386d1c7cd390
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2019-08-06 23:49:54 +0200

    Fix prop types

--- src/components/PostMap/prop-types.js
+++ src/components/PostMap/prop-types.js
@@ -2,5 +2,5 @@ import PropTypes from 'prop-types';
 import PostPropType from 'components/Post/prop-types'
 
 export default {
-  items: PropTypes.arrayOf(PostPropType)
+  items: PropTypes.arrayOf(PropTypes.shape(PostPropType))
 }
