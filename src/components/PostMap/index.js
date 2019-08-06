a7d6a18e4012377680bcdf34992f2ea6f5cd3554
Tobias Järvelöv <tobias.jarvelov@gmail.com>
2019-08-07 00:03:19 +0200

    Rewrite with hooks

--- src/components/PostMap/index.js
+++ src/components/PostMap/index.js
@@ -1,36 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import PostMapMarker from 'components/PostMapMarker';
 import { Map, TileLayer } from 'react-leaflet';
 import config from 'config';
+import propTypes from './prop-types'
 import 'leaflet/dist/leaflet.css';
 
 const center = [62.3875, 16.3255556]; // Geographical center of Sweden
 
-class PostMap extends React.Component {
-  constructor(props) {
-    super(props);
+const PostMap = ({ items = [], zoom = 15 }) => {
+  const leafletMap = React.createRef();
+  const url = `https://api.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token=${config.mapbox.accessToken}`
 
-    this.leafletMap = React.createRef();
-
-    this.state = {
-      url: `https://api.mapbox.com/v4/mapbox.streets/{z}/{x}/{y}.png?access_token=${
-        config.mapbox.accessToken
-      }`
-    };
-  }
-
-  componentDidMount() {
-    setTimeout(() => {
-      this.fitBoundsToMarkers();
-    }, 100);
-  }
-
-  componentDidUpdate() {
-    this.fitBoundsToMarkers();
-  }
-
-  getBounds() {
-    return this.props.items.reduce((bounds, item) => {
+  const getBounds = () => {
+    return items.reduce((bounds, item) => {
       const [longitude, latitude] = item.address.location.coordinates;
       if (longitude && latitude) {
         return [...bounds, [latitude, longitude]];
@@ -40,37 +22,44 @@ class PostMap extends React.Component {
     }, []);
   }
 
-  fitBoundsToMarkers() {
-    if (this.leafletMap.current) {
-      const bounds = this.getBounds();
-      this.leafletMap.current.leafletElement.fitBounds(bounds, {
-        maxZoom: this.state.zoom || this.state.maxZoom || 15
+  const fitBoundsToMarkers = () => {
+    if (leafletMap.current) {
+      const bounds = getBounds();
+
+      leafletMap.current.leafletElement.fitBounds(bounds, {
+        maxZoom: zoom
       });
     }
   }
 
-  render() {
-    return (
-      <div
+  useEffect(() => {
+    if (items.length > 0) {
+      fitBoundsToMarkers()
+    }
+  })
+
+  return (
+    <div
+      style={{
+        height: '400px'
+      }}
+    >
+      <Map
+        ref={leafletMap}
+        center={center}
         style={{
           height: '400px'
         }}
       >
-        <Map
-          ref={this.leafletMap}
-          center={center}
-          style={{
-            height: '400px'
-          }}
-        >
-          <TileLayer attribution="" url={this.state.url} />
-          {this.props.items.map(item => (
-            <PostMapMarker key={item._id} {...item} />
-          ))}
-        </Map>
-      </div>
-    );
-  }
+        <TileLayer attribution="" url={url} />
+        {items.map(item => (
+          <PostMapMarker key={item._id} {...item} />
+        ))}
+      </Map>
+    </div>
+  );
 }
+
+PostMap.propTypes = propTypes;
 
 export default PostMap;
